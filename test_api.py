e3e20a5a88ba2af9caf89471fbf3dfd700ffe81a
Maxime <m.delgerie@gmail.com>
2025-05-11 01:30:56 +0200

    modifications des tests

--- test_api.py
+++ test_api.py
@@ -1,29 +1,20 @@
 import pytest
-from transformers import BertTokenizer, BertForSequenceClassification
-import torch
-from app import predict  # nom du fichier sans l'extension .py
+from app import predict
 
-# Charger le modèle et le tokenizer dans un fixture pytest
-@pytest.fixture(scope="module")
-def setup_model():
-    model_name = "Muselion/Test_BERT"
-    tokenizer = BertTokenizer.from_pretrained(model_name)
-    bert_model = BertForSequenceClassification.from_pretrained(model_name)
-    return tokenizer, bert_model
-
-# Test de la fonction predict
-def test_predict(setup_model):
-    tokenizer, bert_model = setup_model
-    
-    # Exemple de texte à tester
+# Test unitaire pour la fonction predict
+def test_predict():
     test_text = "Here is an example text mentioning Python and Linux"
-    
-    # Appel de la fonction
     result = predict(test_text)
-    
-    # Assertions (par exemple vérifier que les classes prédites contiennent 'python' et 'linux')
-    assert "python" in result, f"Expected 'python' to be in {result}"
-    assert "linux" in result, f"Expected 'linux' to be in {result}"
+    assert isinstance(result, str)
+    assert "python" in result
+    assert "linux" in result
+    assert len(result) > 0
 
-    # Vérifier que la sortie est bien une chaîne de caractères
-    assert isinstance(result, str), f"Expected output to be of type str, but got {type(result)}"
+# Test d'intégration pour la fonction predict
+def test_integration_predict():
+    test_text = "Here is an example text mentioning Python and Linux"
+    result = predict(test_text)
+    assert isinstance(result, str)
+    assert "python" in result
+    assert "linux" in result
+    assert len(result) > 0
